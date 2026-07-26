1d3c3e00401ba9ad7c24451b0bfa9152e8bdea85
Kush Padalia <kushpadalia2002@gmail.com>
2026-07-26 19:38:04 -0400

    Feature: Add Custom PATH Resolution (#8)
    * Feature: Add Custom PATH Resolution
    
    * CI: Fix Headers

--- src/executor.c
+++ src/executor.c
@@ -3,6 +3,7 @@
 #include "../include/executor.h"
 
 #include <errno.h>
+#include <limits.h>
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
@@ -13,6 +14,56 @@
 #include "../include/context.h"
 #include "../include/parser.h"
 #include "../include/utils.h"
+
+static void custom_execvp(const char *file, char *const *argv) {
+  if (*file == '\0') {
+    errno = ENOENT;
+    return;
+  }
+
+  if (strchr(file, '/') != NULL) {
+    execv(file, argv);
+  } else {
+    char *path = getenv("PATH");
+    if (path == nullptr) {
+      size_t len = confstr(_CS_PATH, nullptr, 0);
+      if (!len) {
+        errno = ENOENT;
+        return;
+      }
+
+      char *buf = malloc(len);
+      if (!buf) {
+        errno = ENOMEM;
+        return;
+      }
+
+      confstr(_CS_PATH, buf, len);
+      path = buf;
+    } else {
+      path = strdup(path);
+      if (!path) {
+        errno = ENOMEM;
+        return;
+      }
+    }
+
+    char *p = path;
+    while (p) {
+      char *colon = strchr(p, ':');
+      if (colon) *colon = '\0';
+
+      char full[PATH_MAX];
+      snprintf(full, sizeof(full), "%s/%s", p, file);
+      execv(full, argv);
+
+      if (!colon) break;
+      p = colon + 1;
+    }
+
+    free(path);
+  }
+}
 
 static int execute_command(struct command *c, struct shell_ctx *ctx) {
   if (!c->argc) return -1;
@@ -36,9 +87,19 @@ static int execute_command(struct command *c, struct shell_ctx *ctx) {
     return WEXITSTATUS(status);
   }
 
-  execvp(c->argv[0], c->argv);
-  fprintf(stderr, "csh: unexpected error: %s\n", strerror(errno));
-  _exit(127);
+  custom_execvp(c->argv[0], c->argv);
+
+  switch (errno) {
+    case ENOENT:
+      fprintf(stderr, "csh: command not found: %s\n", c->argv[0]);
+      _exit(127);
+    case EACCES:
+      fprintf(stderr, "csh: permission denied: %s\n", c->argv[0]);
+      _exit(126);
+    default:
+      fprintf(stderr, "csh: unexpected error: %s\n", strerror(errno));
+      _exit(1);
+  }
 }
 
 int execute(struct pipeline *p, struct shell_ctx *ctx) {
